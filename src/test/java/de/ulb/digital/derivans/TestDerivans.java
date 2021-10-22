ee367342f541f8d3b065ae12ee52f617bd78e281
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2021-10-22 16:08:19 +0200

    [app][fix] new test for max image size

--- src/test/java/de/ulb/digital/derivans/TestDerivans.java
+++ src/test/java/de/ulb/digital/derivans/TestDerivans.java
@@ -121,6 +121,25 @@ public class TestDerivans {
 	}
 
 	@Test
+	@Order(2)
+	void testDerivatesFrom737429Oversize(@TempDir Path tempDir) throws Exception {
+
+		Path pathTarget = arrangeMetaddatenAndImagesFor737429Oversize(tempDir);
+
+		// act
+		DerivansParameter dp = new DerivansParameter();
+		dp.setPathInput(pathTarget.resolve("737429.xml"));
+		DerivansConfiguration dc = new DerivansConfiguration(dp);
+		Derivans derivans = new Derivans(dc);
+		derivans.create();
+
+		// assert
+		Path pdfWritten = pathTarget.resolve("191092622.pdf");
+		assertTrue(Files.exists(pdfWritten));
+	}
+
+
+	@Test
 	@Order(3)
 	void testDerivatesOnlyWithPath(@TempDir Path tempDir) throws Exception {
 
@@ -261,6 +280,20 @@ public class TestDerivans {
 		generateJpgsFromList(imagePath, 2367, 3737, List.of("737434", "737436", "737437", "737438"));
 		return pathTarget;
 	}
+
+	public static Path arrangeMetaddatenAndImagesFor737429Oversize(Path tempDir) throws IOException {
+		Path pathTarget = tempDir.resolve("737429");
+		if (Files.exists(pathTarget)) {
+			Files.walk(pathTarget).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+			Files.delete(pathTarget);
+		}
+		Files.createDirectory(pathTarget);
+		Path metsTarget = pathTarget.resolve("737429.xml");
+		Files.copy(TestResource.HD_Aa_737429.get(), metsTarget);
+		Path imagePath = pathTarget.resolve("MAX");
+		generateJpgsFromList(imagePath, 15367, 3737, List.of("737434", "737436", "737437", "737438"));
+		return pathTarget;
+	}	
 
 	public static void generateJpgs(Path imageDir, int width, int height, int number) throws IOException {
 		if (Files.exists(imageDir)) {
